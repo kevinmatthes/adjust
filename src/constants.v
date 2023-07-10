83dc08930fb4226cdcfcb2bace834f9d8e433fb4
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-07-10 08:38:42 +0200

    Added ::= colour for Git configuration files

--- src/constants.v
+++ src/constants.v
@@ -22,16 +22,17 @@ module main
 import term.ui as terminal
 
 const (
-	black             = terminal.Color{0x00, 0x00, 0x00}
-	konsole_green     = terminal.Color{0x11, 0xD1, 0x16}
-	linguist_json5    = terminal.Color{0x26, 0x7C, 0xB9}
-	linguist_markdown = terminal.Color{0x08, 0x3F, 0xA1}
-	linguist_nim      = terminal.Color{0xFF, 0xC2, 0x00}
-	linguist_rust     = terminal.Color{0xDE, 0xA5, 0x84}
-	linguist_tex      = terminal.Color{0x3D, 0x61, 0x17}
-	linguist_v        = terminal.Color{0x4F, 0x87, 0xC4}
-	linguist_yaml     = terminal.Color{0xCB, 0x17, 0x1E}
-	white             = terminal.Color{0xFF, 0xFF, 0xFF}
+	black              = terminal.Color{0x00, 0x00, 0x00}
+	konsole_green      = terminal.Color{0x11, 0xD1, 0x16}
+	linguist_gitconfig = terminal.Color{0xF4, 0x4D, 0x27}
+	linguist_json5     = terminal.Color{0x26, 0x7C, 0xB9}
+	linguist_markdown  = terminal.Color{0x08, 0x3F, 0xA1}
+	linguist_nim       = terminal.Color{0xFF, 0xC2, 0x00}
+	linguist_rust      = terminal.Color{0xDE, 0xA5, 0x84}
+	linguist_tex       = terminal.Color{0x3D, 0x61, 0x17}
+	linguist_v         = terminal.Color{0x4F, 0x87, 0xC4}
+	linguist_yaml      = terminal.Color{0xCB, 0x17, 0x1E}
+	white              = terminal.Color{0xFF, 0xFF, 0xFF}
 )
 
 const help_message = 'Yet another text editor for the terminal, written in V.
