3fe18005b82187203e7564a6871c8ad63b643612
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2023-07-11 14:44:53 +0200

    [Enhancement] Add Key Bindings for F2, F5, F8, and F10 (#30)
    * Update adjust.tex
    
    * Added ::= mcedit-like keybindings (F2, F8, F10)
    
    * Added ::= delete current line with F8
    
    * Create summary of recent changes
    
    * Update 20230711_124011_GitHub_Actions_save-on-f2.ron
    
    ---------
    
    Co-authored-by: GitHub Actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/event_loop.v
+++ src/event_loop.v
@@ -23,121 +23,151 @@ import term.ui { Event }
 
 fn event_loop(event &Event, mut adjust Adjust) {
 	if event.typ == .key_down {
-		match adjust.mode {
-			.command {
-				match event.code {
-					.backspace {
-						mut runes := adjust.command_buffer.runes()
+		if !event_loop_always(event, mut adjust) {
+			match adjust.mode {
+				.command {
+					match event.code {
+						.backspace {
+							mut runes := adjust.command_buffer.runes()
 
-						if runes.len > 1 {
-							runes.delete_last()
-							adjust.command_buffer = runes.string()
+							if runes.len > 1 {
+								runes.delete_last()
+								adjust.command_buffer = runes.string()
+							}
+						}
+						.enter {
+							adjust.execute_command()
+						}
+						.escape {
+							adjust.mode = .view
+							adjust.command_buffer = ''
+						}
+						else {
+							adjust.command_buffer += event.utf8
 						}
 					}
-					.enter {
-						adjust.execute_command()
-					}
-					.escape {
-						adjust.mode = .view
-						adjust.command_buffer = ''
-					}
-					else {
-						adjust.command_buffer += event.utf8
+				}
+				.insert {
+					match event.code {
+						.backspace {
+							adjust.remove_text(.backspace)
+						}
+						.delete {
+							adjust.remove_text(.delete)
+						}
+						.down {
+							adjust.move_cursor_down()
+						}
+						.end {
+							adjust.move_cursor_end()
+						}
+						.enter {
+							adjust.split_line()
+							adjust.move_cursor_start()
+							adjust.move_cursor_down()
+							adjust.v.align()
+						}
+						.escape {
+							adjust.mode = .view
+						}
+						.home {
+							adjust.move_cursor_start()
+						}
+						.left {
+							adjust.move_cursor_left()
+						}
+						.page_down {
+							adjust.move_cursor_page_down()
+						}
+						.page_up {
+							adjust.move_cursor_page_up()
+						}
+						.right {
+							adjust.move_cursor_right()
+						}
+						.tab {
+							adjust.insert_text(' '.repeat(adjust.l.tab))
+						}
+						.up {
+							adjust.move_cursor_up()
+						}
+						else {
+							adjust.insert_text(event.utf8)
+						}
 					}
 				}
-			}
-			.insert {
-				match event.code {
-					.backspace {
-						adjust.remove_text(.backspace)
+				.view {
+					match event.code {
+						.colon {
+							adjust.mode = .command
+							adjust.command_buffer += ':'
+						}
+						.down {
+							adjust.move_cursor_down()
+						}
+						.end {
+							adjust.move_cursor_end()
+						}
+						.greater_than {
+							adjust.go_to_next_file()
+						}
+						.home {
+							adjust.move_cursor_start()
+						}
+						.i {
+							adjust.mode = .insert
+						}
+						.left {
+							adjust.move_cursor_left()
+						}
+						.less_than {
+							adjust.go_to_previous_file()
+						}
+						.page_down {
+							adjust.move_cursor_page_down()
+						}
+						.page_up {
+							adjust.move_cursor_page_up()
+						}
+						.right {
+							adjust.move_cursor_right()
+						}
+						.up {
+							adjust.move_cursor_up()
+						}
+						else {}
 					}
-					.delete {
-						adjust.remove_text(.delete)
-					}
-					.down {
-						adjust.move_cursor_down()
-					}
-					.end {
-						adjust.move_cursor_end()
-					}
-					.enter {
-						adjust.split_line()
-						adjust.move_cursor_start()
-						adjust.move_cursor_down()
-						adjust.v.align()
-					}
-					.escape {
-						adjust.mode = .view
-					}
-					.home {
-						adjust.move_cursor_start()
-					}
-					.left {
-						adjust.move_cursor_left()
-					}
-					.page_down {
-						adjust.move_cursor_page_down()
-					}
-					.page_up {
-						adjust.move_cursor_page_up()
-					}
-					.right {
-						adjust.move_cursor_right()
-					}
-					.tab {
-						adjust.insert_text(' '.repeat(adjust.l.tab))
-					}
-					.up {
-						adjust.move_cursor_up()
-					}
-					else {
-						adjust.insert_text(event.utf8)
-					}
-				}
-			}
-			.view {
-				match event.code {
-					.colon {
-						adjust.mode = .command
-						adjust.command_buffer += ':'
-					}
-					.down {
-						adjust.move_cursor_down()
-					}
-					.end {
-						adjust.move_cursor_end()
-					}
-					.greater_than {
-						adjust.go_to_next_file()
-					}
-					.home {
-						adjust.move_cursor_start()
-					}
-					.i {
-						adjust.mode = .insert
-					}
-					.left {
-						adjust.move_cursor_left()
-					}
-					.less_than {
-						adjust.go_to_previous_file()
-					}
-					.page_down {
-						adjust.move_cursor_page_down()
-					}
-					.page_up {
-						adjust.move_cursor_page_up()
-					}
-					.right {
-						adjust.move_cursor_right()
-					}
-					.up {
-						adjust.move_cursor_up()
-					}
-					else {}
 				}
 			}
 		}
+	}
+}
+
+fn event_loop_always(e &Event, mut a Adjust) bool {
+	return if e.typ == .key_down {
+		match e.code {
+			.f2 {
+				a.save_file()
+				true
+			}
+			.f5 {
+				a.reformat_file()
+				true
+			}
+			.f8 {
+				a.delete_current_line()
+				true
+			}
+			.f10 {
+				a.quit()
+				true
+			}
+			else {
+				false
+			}
+		}
+	} else {
+		false
 	}
 }
 
